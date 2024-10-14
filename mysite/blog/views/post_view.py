d6ebdf430559b43bc9636ce993dcb2b84899361c
oLucas-Cipriano <lucas.e.a.cipriano@gmail.com>
2024-10-14 10:11:56 -0300

    mas uma aula com uma anomalia sem didatica

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
 from django.views import generic
 
+from blog.models.post import Post
 
-class PostView(generic.View):
-    def get(self, resquest, *args, **kwargs):
-        return HttpResponse('hell')
+
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
